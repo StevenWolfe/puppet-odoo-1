b4d55335b7ce86bf96384a04dae36646d6ea58d1
Brandon Haymore <bhaymore@local-motors.com>
2015-06-12 15:06:40 -0700

    requires newer version of wkhtmltopdf

--- manifests/install.pp
+++ manifests/install.pp
@@ -14,7 +14,11 @@ class odoo::install inherits odoo {
   # install dependancy packages
   package { $odoo::dependancy_packages:
     ensure => installed,
-    }
+    } ->
+  exec { 'install wkhtmltopdf':
+    command => "cd /usr/local/src/; wget http://downloads.sourceforge.net/wkhtmltopdf/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb; dpkg -i /usr/local/src/wkhtmltox-0.12.2.1_linux-trusty-amd64.deb",
+    unless  => "dpkg -l|grep wkhtmltopdf",
+  }
   exec { 'odoo_pip_requirements_install':
     command => "/usr/bin/pip install -r ${odoo::install_path}/requirements.txt",
     require => [Vcsrepo[$odoo::install_path],Package['python-pip']],
